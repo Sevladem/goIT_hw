49f079264bff8c9aade7fbc40cd9804e696c062a
sdemenev <sevladem@gmail.com>
2017-01-27 13:20:16 +0200

    first commit

---
+++ src/hm1/Fifth.java
@@ -0,0 +1,13 @@
+package hm1;
+
+/**
+ * Created by SDem on 27.01.2017.
+ */
+public class Fifth {
+
+    public static void main(String[] args) {
+
+        System.out.println("Мой первый класс на Java");
+
+    }
+}
