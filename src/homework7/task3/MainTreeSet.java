2f1c28d34707bf307a859ae7e0e7b85077e67319
Sergey <sevladem@gmail.com>
2017-03-29 14:54:41 +0300

    homework 7 six commit

--- src/homework7/task3/MainTreeSet.java
+++ src/homework7/task3/MainTreeSet.java
@@ -9,7 +9,7 @@ import java.util.*;
 public class MainTreeSet {
 
     private static final int COUNT_OF_ORDER = 10;
-    private static final Random rand = new Random();
+    private static final Random RAND = new Random();
     public static final String LASTNAME_FOR_FIND = "Petrov";
     public static final String MESSAGE_WHEN_NOT_FIND = "In the set there is no order with the user by the lastname %s";
     public static final String MESSAGE_WHEN_FIND = "In the set there is an order with the user by the lastname %s";
@@ -66,8 +66,8 @@ public class MainTreeSet {
 
         for (int i = 0; i < COUNT_OF_ORDER-2; i++) {
 
-            int randIndexUser = rand.nextInt(100) /10;
-            int randIndexOrder = rand.nextInt(500) /10;
+            int randIndexUser = RAND.nextInt(100) /10;
+            int randIndexOrder = RAND.nextInt(500) /10;
 
             User user = new User(i,"FN"+randIndexUser,randIndexUser<2?"Petrov":"LN"+randIndexUser, "city"+randIndexUser,5000+randIndexUser);
             Order order = new Order(i,100+randIndexOrder, Currency.getInstance(randIndexOrder<25?"UAH":"USD"), "itemName"+randIndexOrder, "shopID"+randIndexOrder,user );
